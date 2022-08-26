9c0a1606b806f7860958859feb47819707de3d14
baba-s <shotababa1206@gmail.com>
2022-08-26 19:57:40 +0900

    first commit

---
+++ Editor/PackageManagerOpenButtonExtension.cs
@@ -0,0 +1,75 @@
+﻿using Unity.CodeEditor;
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine.UIElements;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace Kogane.Internal
+{
+    [InitializeOnLoad]
+    internal sealed class PackageManagerOpenButtonExtension
+        : VisualElement,
+          IPackageManagerExtension
+    {
+        private bool        m_isInitialized;
+        private PackageInfo m_selectedPackageInfo;
+
+        static PackageManagerOpenButtonExtension()
+        {
+            var extension = new PackageManagerOpenButtonExtension();
+            PackageManagerExtensions.RegisterExtension( extension );
+        }
+
+        VisualElement IPackageManagerExtension.CreateExtensionUI()
+        {
+            m_isInitialized = false;
+            return this;
+        }
+
+        void IPackageManagerExtension.OnPackageSelectionChange( PackageInfo packageInfo )
+        {
+            Initialize();
+
+            m_selectedPackageInfo = packageInfo;
+        }
+
+        private void Initialize()
+        {
+            if ( m_isInitialized ) return;
+
+            VisualElement root = this;
+
+            while ( root is { parent: { } } )
+            {
+                root = root.parent;
+            }
+
+            var openButton = new Button
+            (
+                () =>
+                {
+                    var assetPath       = m_selectedPackageInfo.assetPath;
+                    var packageJsonPath = $"{assetPath}/package.json";
+
+                    CodeEditor.CurrentEditor.OpenProject( packageJsonPath );
+                }
+            )
+            {
+                text = "Open",
+            };
+
+            var removeButton = root.FindElement( x => x.name == "PackageRemoveCustomButton" );
+            removeButton.parent.Insert( 0, openButton );
+
+            m_isInitialized = true;
+        }
+
+        void IPackageManagerExtension.OnPackageAddedOrUpdated( PackageInfo packageInfo )
+        {
+        }
+
+        void IPackageManagerExtension.OnPackageRemoved( PackageInfo packageInfo )
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Kogane Package Manager Ping Button Extension
+
+package.json をコードエディタで開く Open ボタンを Package Manager に追加するエディタ拡張
